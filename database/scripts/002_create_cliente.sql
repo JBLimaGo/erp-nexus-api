4c021170cbaf1cb1ef25c60e655873c0a37dd7a2
jblimago <jbldesenvolvimento@gmail.com>
2026-07-23 15:01:07 -0300

    tarefa: adicionar scripts de banco de dados Firebird

---
+++ database/scripts/002_create_cliente.sql
@@ -0,0 +1,27 @@
+INSERT INTO CUSTOMER (
+    NAME,
+    DOCUMENT,
+    EMAIL,
+    ACTIVE
+)
+VALUES (
+    'Empresa Alpha Ltda',
+    '12345678000190',
+    'contato@empresaalpha.com.br',
+    TRUE
+);
+
+INSERT INTO CUSTOMER (
+    NAME,
+    DOCUMENT,
+    EMAIL,
+    ACTIVE
+)
+VALUES (
+    'Comercial Beta Ltda',
+    '98765432000110',
+    'contato@comercialbeta.com.br',
+    TRUE
+);
+
+COMMIT;
